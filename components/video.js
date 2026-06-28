630a5ba92c695b01bff021ea0f9f0d41431e3afa
mathsaveccecile <tetiloug@gmail.com>
2026-06-28 18:25:05 +0200

    Update video.js

--- components/video.js
+++ components/video.js
@@ -1,1 +1,12 @@
-components/video.js
+function renderVideo(step){
+  if(!step.youtube){
+    return `<div class="placeholder">🎥<br>Vidéo à ajouter</div>`;
+  }
+
+  return `
+    <iframe class="video-frame"
+      src="https://www.youtube.com/embed/${step.youtube}"
+      allowfullscreen>
+    </iframe>
+  `;
+}
