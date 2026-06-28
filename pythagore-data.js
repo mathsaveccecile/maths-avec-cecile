8949aef8e3f5a8330ed64ad1378698321d44e0be
mathsaveccecile <tetiloug@gmail.com>
2026-06-28 17:45:17 +0200

    Create pythagore-data.js

---
+++ pythagore-data.js
@@ -0,0 +1,33 @@
+const capsuleData = {
+  title: "Pythagore",
+  levels: "4e / 3e",
+  duration: "18 min",
+  steps: [
+    {
+      type: "video",
+      title: "0. Introduction",
+      youtube: ""
+    },
+    {
+      type: "fiche",
+      title: "Fiche 1",
+      file: ""
+    },
+    {
+      type: "image",
+      title: "Observer avant de calculer",
+      image: ""
+    },
+    {
+      type: "quiz",
+      title: "Quiz 1",
+      question: "Peut-on utiliser Pythagore dans n'importe quel triangle ?",
+      options: [
+        "Oui, toujours",
+        "Non, seulement dans un triangle rectangle",
+        "Seulement dans un triangle isocèle"
+      ],
+      answer: 1
+    }
+  ]
+};
